47fa952503afe9dad4db312b396ba652c03cabd4
Karen Hambardzumyan <mahnerak@gmail.com>
2025-08-18 21:11:38 +0000

    Fix Cython version missing from PyPI

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
     name="aimrocks",
     version='0.5.2',
     description='RocksDB wrapper implemented in Cython.',
-    setup_requires=['setuptools>=25', 'Cython>=3.0.0a9'],
+    setup_requires=['setuptools>=25', 'Cython==3.0.12'],
     packages=find_packages('./src'),
     package_dir={'': 'src'},
     package_data={'aimrocks': ['src/*']},
